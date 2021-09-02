13dce601b38afcc71d4b9f5179a091bc79b2ae4a
emily <emil7809@gmail.com>
2021-09-02 23:59:41 +0200

    im tired and going to bed

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 "use strict";
 
+let iterator;
 const queueSize = getNumberOfCostumers();
 
 function getNumberOfCostumers(){
     return Math.floor(Math.random()*32);
 }
 
+bar();
 
+function bar() {
 
 for (let i = 0; i <= 32; i += queueSize){
     let bar = document.createElement("div");
@@ -14,3 +17,17 @@ for (let i = 0; i <= 32; i += queueSize){
     bar.style.height = `${i}px`;
     document.querySelector("#bar_id").appendChild(bar);
  }
+
+ loop()
+
+}
+
+function loop() {
+    iterator++;
+
+    if (iterator < bar) {
+       
+        document.querySelector("#bar_id").appendChild(bar);
+        setTimeout(loop, 300);
+    }
+}
